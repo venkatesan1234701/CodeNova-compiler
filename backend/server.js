d6f8bdd12a6734db6020116c36d0b7f452552e7e
venkatesan1234701 <amma13066@gmail.com.com>
2026-03-06 00:12:51 +0530

    fix port for render

--- backend/server.js
+++ backend/server.js
@@ -131,6 +131,12 @@ error:"Server error"
 
 })
 
-app.listen(5000,()=>{
-console.log("🚀 Server running on port 5000")
+// app.listen(5000,()=>{
+// console.log("🚀 Server running on port 5000")
+// })
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT,()=>{
+console.log("🚀 Server running on port " + PORT)
 })
